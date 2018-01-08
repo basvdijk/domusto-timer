0a5d9e2abab30eabb0a3591c7f00f67db4a85cc4
Bas van Dijk <bas@usto.nl>
2018-01-08 16:23:39 +0100

    Implemented timers

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import config from '../../config';
 // DOMUSTO
 import DomustoPlugin from '../../domusto/DomustoPlugin';
 import DomustoSignalHub from '../../domusto/DomustoSignalHub';
-import DomustoDevicesManager from '../../domusto/DomustoDevicesManager';
+import DomustoDevice from '../../domusto/DomustoDevice';
 
 // INTERFACES
 import { Domusto } from '../../domusto/DomustoInterfaces';
@@ -30,18 +30,169 @@ class DomustoTimer extends DomustoPlugin {
     constructor(pluginConfiguration: Domusto.PluginConfiguration) {
 
         super({
-            plugin: 'timers',
+            plugin: 'timer',
             author: 'Bas van Dijk',
             category: Domusto.PluginCategories.system,
             version: '0.0.1',
             website: 'http://domusto.com'
         });
 
-        DomustoDevicesManager.getDevicesByRole(Domusto.DeviceRole.output);
+        for (let d of config.devices) {
+
+            let device = new DomustoDevice(d);
+
+            // Initialise timers when specified
+            if (device.pluginSettings.timer) {
+
+                this.console.header('INITIALISING TIMERS for', device.id);
+
+                for (let timer of device.pluginSettings.timer) {
+
+                    if (timer.enabled) {
+                        this.initTimer(device, timer);
+                    } else {
+                        this.console.warning(`    Timer (${timer.type})  disabled for ${device.id} state ${timer.state}`);
+                    }
+
+                }
+
+            }
+        }
 
     }
 
-    onSignalReceivedForPlugin(signal: Domusto.Signal) {
+    initTimer(device, timer) {
+
+        let _device = device;
+
+        switch (timer.type) {
+
+            case 'time':
+                this.console.log('    Timer (time)  enabled  for', device.id, 'state', timer.state, 'at', timer.time);
+
+                let job = schedule.scheduleJob(timer.time, () => {
+                    this.console.log('     Timer (time)  activated for', device.id, 'state', timer.state);
+
+                    this.broadcastSignal(device.plugin.deviceId, {
+                        state: timer.state
+                    }, Domusto.SignalSender.client, device.plugin.id);
+
+                });
+
+                break;
+
+            case 'sun':
+                this.scheduleSunTimer(device, timer);
+                break;
+
+            case 'event':
+                this.initEventTimer(device, timer);
+                break;
+
+        }
+
+    }
+
+    /**
+     * Schedules a timer according to sunset, sunrise etc
+     * @param {object} device The device who executes the command
+     * @param {object} timer The timer object which contains the timer information
+     */
+    scheduleSunTimer(device, timer) {
+
+        let _device = device;
+        let _timer = timer;
+
+        let times = SunCalc.getTimes(new Date(), config.location.latitude, config.location.longitude);
+        let date = this._offsetDate(times[_timer.time], _timer.offset);
+
+        // If the next event is tomorrow
+        if (date < new Date()) {
+            let today = new Date();
+            let tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000));
+            times = SunCalc.getTimes(tomorrow, config.location.latitude, config.location.longitude);
+            date = this._offsetDate(times[_timer.time], _timer.offset);
+        }
+
+        this.console.log('    Timer (sun)   enabled  for', _device.id, 'state', _timer.state, 'at', date, '/', new Date(date).toLocaleString());
+
+        schedule.scheduleJob(date, () => {
+            this.console.log('     Timer (sun)  activated for', _device.id, 'state', _timer.state);
+
+            this.console.log(device.plugin.deviceId, {
+                pluginId: device.plugin.id,
+                state: timer.state
+            });
+
+            this.broadcastSignal(device.plugin.deviceId, {
+                state: timer.state
+            }, Domusto.SignalSender.client, device.plugin.id);
+
+
+            // Reschedule for next day
+            this.scheduleSunTimer(_device, _timer);
+        });
+
+    }
+
+    /**
+     * Schedules a timer according to sunset, sunrise etc
+     * @param {object} device The device who executes the command
+     * @param {object} timer The timer object which contains the timer information
+     */
+    initEventTimer(device, timer) {
+
+        let _device = device;
+        let _timer = timer;
+
+        let date = this._offsetDate(new Date(), _timer.offset);
+
+        schedule.scheduleJob(date, () => {
+            this.console.log('   Timer (event) activated for', _device.id, 'state', _timer.state);
+            // util.logTimersToFile('Timer (event) activated for ' + _device.id + ' state: ' + _timer.state);
+
+            this.broadcastSignal(device.plugin.deviceId, {
+                state: timer.state
+            }, Domusto.SignalSender.client, device.plugin.id);
+
+        });
+
+    }
+
+    /**
+ * Offsets the given date with the specified offset in cron format
+ * @param {string} cronData Date in the cron format e.g "* 10 * * * *" to offset 10 minutes
+ */
+    _offsetDate(date, cronDateOffset) {
+
+        if (cronDateOffset) {
+
+            let cronDateSplitted = cronDateOffset.split(' ');
+
+            if (cronDateSplitted.length !== 6) {
+                console.error('invalid cron formatted date pattern');
+            }
+
+            let secondsOffset = parseInt(cronDateSplitted[0]) || 0;
+            date.setSeconds(date.getSeconds() + secondsOffset);
+
+            let minuteOffset = parseInt(cronDateSplitted[1]) || 0;
+            date.setMinutes(date.getMinutes() + minuteOffset);
+
+            let hourOffset = parseInt(cronDateSplitted[2]) || 0;
+            date.setHours(date.getHours() + hourOffset);
+
+            let dayOffset = parseInt(cronDateSplitted[3]) || 0;
+            date.setDate(date.getDate() + dayOffset);
+
+            let monthOffset = parseInt(cronDateSplitted[4]) || 0;
+            date.setMonth(date.getMonth() + monthOffset);
+
+            let yearOffset = parseInt(cronDateSplitted[5]) || 0;
+            date.setFullYear(date.getFullYear() + yearOffset);
+        }
+
+        return date;
 
     }
 
